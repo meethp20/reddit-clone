8ad1ef1427a15305997f06e3b70c05b1095056cf
Meeth Patel <meeth2060@gmail.com>
2025-06-25 17:34:28 +0530

    added shadcn

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 
 import RedditMobile from "../../../public/reddit-full.svg"
-
+import RedditText from "../../../public/logo-name.svg"
 export function Navbar(){
     return(
     <nav className="h-[10vh] w-full flex justify-between lg:px-14 items-center px-5">
@@ -10,11 +10,17 @@ export function Navbar(){
                 href="/" 
                 className="flex items-center"
                 >
-                <Image
-                 src={RedditMobile} 
-                 className="h-10 w-fit"
-                  alt="mobile reddit blah"/>
-                
+              <Image 
+                src={RedditMobile}
+                className="h-10 w-fit"
+                alt="alt"
+              />
+              <Image 
+                src={RedditText}
+                className="h-9 block w-fit lg:block "
+                alt="Reddit"
+              />
+               
 </Link>
         </nav>
     )
