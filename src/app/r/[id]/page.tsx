33221325fea575b5181003bda32c5374a247d42c
Meeth Patel <meeth2060@gmail.com>
2025-06-27 16:57:59 +0530

    fucked

---
+++ src/app/r/[id]/page.tsx
@@ -0,0 +1,23 @@
+import { Card } from "@/components/ui/card";
+
+
+
+export default function SubRedditPage() {
+
+
+    return (
+    <div className="lg:w-[1000px] flex mx-auto ">
+      <div className="w-[65%] flex flex-col gap-y-5">
+           <h1>hello</h1>
+      </div>
+
+      <div className="w-[35%] ">
+        <Card > 
+            <div className=" pl-3 bg-muted ">
+                About Community
+            </div>
+        </Card>
+      </div>
+    </div>
+  );
+}
