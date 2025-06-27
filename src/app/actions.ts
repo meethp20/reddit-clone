1c0e48714cad97c7419bc096e1a3120b13098b9d
Meeth Patel <meeth2060@gmail.com>
2025-06-27 14:08:51 +0530

    added connection but the subreddit still not working

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,60 +1,57 @@
-'use server'
+"use server";
 
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
 import prisma from "./lib/db";
 import { Prisma } from "@prisma/client";
 
 export async function updateUserName(prevState: any, formData: FormData) {
-    //get data
-    const {getUser} = getKindeServerSession();
-    const user = await getUser();
-   //check if data is empty or not
-    if(!user){
-        return redirect(" /api/auth/creation")
-    }
-    //from form input from username i hope it will be connnected
-    const username = formData.get('username') as string  
-// now update this username in the db and return a message
- try{
-  await prisma.user.update({
-    where:{
-        id:user.id
-    },
-    data:{
-        userName:username,
-    }
-  })
-return{
-    message:"Username updated successfully!",
-    success: true
-}
-
- }catch(e){
-    if(e instanceof Prisma.PrismaClientKnownRequestError){
-        if(e.code ==='P2002'){
-            return {
-                message:"This username is already taken",
-                success: false
-            }
-        }
+  //get data
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  //check if data is empty or not
+  if (!user) {
+    return redirect(" /api/auth/creation");
+  }
+  //from form input from username i hope it will be connnected
+  const username = formData.get("username") as string;
+  // now update this username in the db and return a message
+  try {
+    await prisma.user.update({
+      where: {
+        id: user.id,
+      },
+      data: {
+        userName: username,
+      },
+    });
+    return {
+      message: "Username updated successfully!",
+      success: true,
+    };
+  } catch (e) {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      if (e.code === "P2002") {
+        return {
+          message: "This username is already taken",
+          success: false,
+        };
+      }
     }
     return {
-        message:"An error occurred while updating username",
-        success: false
-    }
- }
+      message: "An error occurred while updating username",
+      success: false,
+    };
+  }
 }
 
+export async function createCommunity(formData: FormData) {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
 
+  if (!user) {
+    return redirect("/api/auth/creation");
+  }
 
-export async function createCommunity(formData:FormData){
-    const {getUser} = getKindeServerSession()
-    const user = await getUser()
-
-    if(!user){
-        return redirect("/api/auth/creation")
-    }
-
-   
+  const data = await prisma;
 }
